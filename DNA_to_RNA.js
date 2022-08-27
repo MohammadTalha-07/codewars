84b25b2245e04a39a05cb004b378ec71f43007da
Tally <talha030@gmail.com>
2022-08-27 22:15:43 +0530

    back on track

---
+++ DNA_to_RNA.js
@@ -0,0 +1,9 @@
+function DNAtoRNA(dna) {
+
+  // create a function which returns an RNA sequence from the given DNA sequence
+//  var str = dna.toLowerCase();
+
+ return (dna.replace(/T/g,"U" ));
+
+
+}
